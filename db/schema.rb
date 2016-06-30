482a221852fc12d13c91c74bd80ff1d4dbdaf070
Huanhuan Huang <huanhuan.huang@betterbills.com>
2016-07-01 00:29:48 +1000

    casein setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630134222) do
+ActiveRecord::Schema.define(version: 20160630142335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
-    t.string   "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "casein_admin_users", force: :cascade do |t|
     t.string   "login",                           null: false
