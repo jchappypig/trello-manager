c6aec214c2c9d0a90494a74626de3c45fe5603dc
Huanhuan Huang <huanhuan.huang@betterbills.com>
2016-07-02 00:46:45 +1000

    able to show cards and calculate points

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+	#Casein routes
+	namespace :casein do
+		resources :cards, :only => [:index, :show]
+	end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
