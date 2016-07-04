bb287aaa0fe05a2960501218848eca1086f7a519
Huanhuan Huang <huanhuan.huang@betterbills.com>
2016-07-04 17:46:00 +1000

    added burn up and team velocity in the navigation

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   get '/burn-up' => 'teams#burn_up'
   get '/team-velocity' =>'teams#velocity'
+  get '/trello' => 'casein/admin_user_sessions#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
