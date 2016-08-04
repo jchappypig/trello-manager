70979eef35f8ed7411cde6a0d9b25991682a7040
Huanhuan Huang <huanhuan.huang@betterbills.com>
2016-08-04 20:25:26 +1000

    remove member trello id

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,9 +1,9 @@
 class Member
-  PHOEBE = {name: 'Phoebe', trello_identifier: '54c9c4df04ebb717eec26520'}.with_indifferent_access
-  PHIL = {name: 'Phil Jacob', trello_identifier: '5448736e75e3bf015aeb8a28'}.with_indifferent_access
-  DAISY = {name: 'Daisy Xu', trello_identifier: '56b957acb9656015e320e127'}.with_indifferent_access
-  JOHNNY = {name: 'Johnny Woo', trello_identifier: '5654291ff267c8e66c2846e56d'}.with_indifferent_access
-  HUAN = {name: 'Huanhuan', trello_identifier: '52ef549539ceeec275e2b8ee'}.with_indifferent_access
+  PHOEBE = {name: 'Phoebe', trello_identifier: ''}.with_indifferent_access
+  PHIL = {name: 'Phil Jacob', trello_identifier: ''}.with_indifferent_access
+  DAISY = {name: 'Daisy Xu', trello_identifier: ''}.with_indifferent_access
+  JOHNNY = {name: 'Johnny Woo', trello_identifier: ''}.with_indifferent_access
+  HUAN = {name: 'Huanhuan', trello_identifier: ''}.with_indifferent_access
 
   ALL = [PHOEBE, PHIL, DAISY, JOHNNY, HUAN]
 
